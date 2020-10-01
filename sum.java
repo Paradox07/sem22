42bafca15fbdf55ea74fa32a515d7c7bff4a0134
Paradox07 <43773624+Paradox07@users.noreply.github.com>
2020-10-01 22:09:49 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ package jswork;
 public class c1 { 
 		static int arr[] = {22,55,65,78,21,77,7,55}; 
 		static int smallest() 
-	{ 
+		{ 
 		 int i; 
 		 int min = arr[0]; 
 		  for (i = 1; i < arr.length; i++) 
